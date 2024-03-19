e0c60d8043577e32935abaded45ea649af8c2366
Kimberly McCarty <mccartykim@zoho.com>
2024-03-19 01:20:37 -0400

    packidge

--- flake.nix
+++ flake.nix
@@ -6,28 +6,39 @@
   };
 
   outputs = { self, nixpkgs, ... }: {
-    packages.x86_64-linux.llamaa = let
-      modelUrl = "llama-2-7b-chat.Q4_K_M.gguf";
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-      in 
-    pkgs.stdenv.mkDerivation {
-    name = "llama-cpp-plus-model";
-        buildInputs = [ pkgs.llama-cpp ]; # Ensure llama-cpp is available
-	src = pkgs.fetchgit {
-        url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF.git";
-        rev = "191239b3e26b2882fb562ffccdd1cf0f65402adb";
-        hash = "sha256-WeRLiFFEDX4lNqHVAh9KnplmKGlpWXSDXlKfu2AatqE=";
-        fetchLFS = true;
-	sparseCheckout = [ modelUrl ];
-      };
+    packages.x86_64-linux = { 
+
+      llama-2-7b-chat-q4-k-m-gguf = pkgs.stdenv.mkDerivation {
+          name = "llama2 7b chat q4 k m gguf";
+	  src = pkgs.fetchgit {
+	    url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF.git";
+	    rev = "191239b3e26b2882fb562ffccdd1cf0f65402adb";
+	    hash = "sha256-WeRLiFFEDX4lNqHVAh9KnplmKGlpWXSDXlKfu2AatqE=";
+	    fetchLFS = true;
+	    sparseCheckout = [ modelUrl ];
+	  };
+
+          installPhase = ''
+	    mk -p $out/share/models
+	    cp llama-2-7b-chat.Q4_K_M.gguf $out
+	  '';
+      }
+      
+      llamaa = let
+	modelUrl = "llama-2-7b-chat.Q4_K_M.gguf";
+	pkgs = import nixpkgs {
+	  system = "x86_64-linux";
+	};
+	in 
+      pkgs.stdenv.mkDerivation {
+      name = "llama-cpp-plus-model";
+	  buildInputs = [ pkgs.llama-cpp ]; # Ensure llama-cpp is available
 
 
         installPhase = ''
           mkdir -p $out/bin
           echo "#!${pkgs.stdenv.shell}" > $out/bin/run-llama-cpp
-          echo "${pkgs.llama-cpp}/bin/llama -m ${modelUrl}" >> $out/bin/run-llama-cpp
+          echo "${pkgs.llama-cpp}/bin/llama -m ${ pkgs.llama-2-7b-chat-q4-k-m-gguf }/llama-2-7b-chat.Q4_K_M.gguf" >> $out/bin/run-llama-cpp
           chmod +x $out/bin/run-llama-cpp
         '';
     };
