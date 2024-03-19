f1f563212c2e772bf74587db3882090f5719cb55
Kimberly McCarty <mccartykim@zoho.com>
2024-03-19 01:37:32 -0400

    Overlays, the last refuge of the damned

--- flake.nix
+++ flake.nix
@@ -6,37 +6,43 @@
   };
 
   outputs = { self, nixpkgs, ... }: {
-    packages.x86_64-linux = rec { 
-
-      llama-2-7b-chat-q4-k-m-gguf = let
-	pkgs = import nixpkgs {
-	  system = "x86_64-linux";
-	};
-      in pkgs.stdenv.mkDerivation {
-          name = "llama2 7b chat q4 k m gguf";
-	  src = pkgs.fetchgit {
-	    url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF.git";
-	    rev = "191239b3e26b2882fb562ffccdd1cf0f65402adb";
-	    hash = "sha256-WeRLiFFEDX4lNqHVAh9KnplmKGlpWXSDXlKfu2AatqE=";
-	    fetchLFS = true;
-	    sparseCheckout = [ "llama-2-7b-chat.Q4_K_M.gguf" ];
+    packages.x86_64-linux = 
+      let model = 
+	let
+	  pkgs = import nixpkgs {
+	    system = "x86_64-linux";
 	  };
+	  in pkgs.stdenv.mkDerivation {
+	    name = "llama2 7b chat q4 k m gguf";
+	    src = pkgs.fetchgit {
+	      url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF.git";
+	      rev = "191239b3e26b2882fb562ffccdd1cf0f65402adb";
+	      hash = "sha256-WeRLiFFEDX4lNqHVAh9KnplmKGlpWXSDXlKfu2AatqE=";
+	      fetchLFS = true;
+	      sparseCheckout = [ "llama-2-7b-chat.Q4_K_M.gguf" ];
+	    };
 
-          installPhase = ''
-	    mkdir -p $out/share/models
-	    cp llama-2-7b-chat.Q4_K_M.gguf $out
-	  '';
-      };
+	    installPhase = ''
+	      mkdir -p $out/share/models
+	      cp llama-2-7b-chat.Q4_K_M.gguf $out
+	    '';
+	};
+	in
+      llama-2-7b-chat-q4-k-m-gguf = model;
       
       llamaa = let
 	modelUrl = "llama-2-7b-chat.Q4_K_M.gguf";
 	pkgs = import nixpkgs {
 	  system = "x86_64-linux";
+	  pkgs.llama-2-7b-chat-q4-k-m-gguf = model 
 	};
 	in 
       pkgs.stdenv.mkDerivation {
       name = "llama-cpp-plus-model";
-	  buildInputs = [ pkgs.llama-cpp pkgs.llama-2-7b-chat-q4-k-m-gguf ]; # Ensure llama-cpp is available
+	  buildInputs = [ 
+	  pkgs.llama-cpp 
+	  pkgs.llama-2-7b-chat-q4-k-m-gguf 
+	  ]; # Ensure llama-cpp is available
 
 
         installPhase = ''
