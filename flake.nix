da4c33e6290319b4a266f56d431315d3d7e7b15f
Kimberly McCarty <mccartykim@zoho.com>
2024-03-19 02:28:08 -0400

    once more

--- flake.nix
+++ flake.nix
@@ -7,45 +7,38 @@
 
   outputs = { self, nixpkgs, ... }: {
     packages.x86_64-linux = 
-      let model = 
-	let
-	  pkgs = import nixpkgs {
-	    system = "x86_64-linux";
+      let 
+	modelUrl = "llama-2-7b-chat.Q4_K_M.gguf";
+	pkgs = import nixpkgs {
+	  system = "x86_64-linux";
+	};
+        model = pkgs.stdenv.mkDerivation {
+	  name = "llama2 7b chat q4 k m gguf";
+	  src = pkgs.fetchgit {
+	    url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF.git";
+	    rev = "191239b3e26b2882fb562ffccdd1cf0f65402adb";
+	    hash = "sha256-WeRLiFFEDX4lNqHVAh9KnplmKGlpWXSDXlKfu2AatqE=";
+	    fetchLFS = true;
+	    sparseCheckout = [ "llama-2-7b-chat.Q4_K_M.gguf" ];
 	  };
-	  in pkgs.stdenv.mkDerivation {
-	    name = "llama2 7b chat q4 k m gguf";
-	    src = pkgs.fetchgit {
-	      url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF.git";
-	      rev = "191239b3e26b2882fb562ffccdd1cf0f65402adb";
-	      hash = "sha256-WeRLiFFEDX4lNqHVAh9KnplmKGlpWXSDXlKfu2AatqE=";
-	      fetchLFS = true;
-	      sparseCheckout = [ "llama-2-7b-chat.Q4_K_M.gguf" ];
-	    };
 
-	    installPhase = ''
-	      mkdir -p $out/share/gguf
-	      cp llama-2-7b-chat.Q4_K_M.gguf $out/share/gguf/
-	    '';
+	  installPhase = ''
+	    mkdir -p $out/share/gguf
+	    cp llama-2-7b-chat.Q4_K_M.gguf $out/share/gguf/
+	  '';
 	};
 	in rec {
       llama-2-7b-chat-q4-k-m-gguf = model;
       
-      llamaa = let
-	modelUrl = "llama-2-7b-chat.Q4_K_M.gguf";
-	pkgs = import nixpkgs {
-	  system = "x86_64-linux";
-	  nixpkgs.x86_64-linux.llama-2-7b-chat-q4-k-m-gguf = model;
-	};
-	in 
-      pkgs.writeShellApplication {
-      name = "llama-cpp-plus-model";
+      llamaa = pkgs.writeShellApplication {
+	name = "llama-cpp-plus-model";
 	  runtimeInputs = [ 
 	  pkgs.llama-cpp 
 	  model
 	  ]; # Ensure llama-cpp is available
 
 
-        text = "${pkgs.llama-cpp}/bin/llama -m ${model}/share/gguf/llama-2-7b-chat.Q4_K_M.gguf";
+        text = "${pkgs.llama-cpp}/bin/llama -m ${ model }/share/gguf/llama-2-7b-chat.Q4_K_M.gguf";
     };
   };
  };
